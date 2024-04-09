1d2eec0cf793000cfe8bd25e68ac5c1918838722
Aleksey Efimov <a.efimov@infinet.ru>
2024-04-09 14:52:03 +0500

    init

---
+++ gpt.py
@@ -0,0 +1,14 @@
+import openai
+import os
+from gigachat import GigaChat
+
+GPT_TOKEN = os.getenv('GPT_TOKEN')
+
+def gpt_summarise(text):
+    with GigaChat(credentials=GPT_TOKEN, verify_ssl_certs=False) as giga:
+        print(text)
+        response = giga.chat(text)
+        print(response.choices[0].message.content)
+        return response.choices[0].message.content
+
+
