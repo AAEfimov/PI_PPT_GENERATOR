03be396fe6cbd6c99ab84f38501d33709b7fb670
Alexandra <alex-sacha.01@mail.ru>
2024-05-22 15:48:53 +0500

    fix function for pdf format

--- server/streamlit_api.py
+++ server/streamlit_api.py
@@ -99,7 +99,7 @@ def exec_p():
                 label="Download pptx", data=binary_output.getvalue(), file_name=f"{filename}.pptx"
             )
         elif file_format == "pdf":
-            binary_output = presentate_pdf(x, img)
+            binary_output = text2ppt.presentate(x, img)
             sl.download_button(
                 label="Download pdf", data=binary_output.getvalue(), file_name=f"{filename}.pdf"
             )
